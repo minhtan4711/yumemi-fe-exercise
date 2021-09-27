ff8f55b3de080178f7e940262fbd16fa2476555e
minhtan4711 <minhtan471100@gmail.com>
2021-09-27 11:02:25 +0700

    refactor: delete unused package

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react'
 import HighchartsReact from 'highcharts-react-official'
 import Highcharts from 'highcharts'
-import _ from 'lodash'
 import resas from './api/resas'
 
 const App = () => {
